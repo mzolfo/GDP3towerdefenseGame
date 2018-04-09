83c388f4afa77177285a001b8bb5337437737d3c
Matt Zolfo <Matt.Zolfo@iam.local>
2018-04-09 14:29:01 -0500

    added everything that has been made so far

---
+++ AGESProject2/Assets/Script/EnemyMovement.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    
+
+    
+
+    public GameObject TargetPlayer;
+    
+
+    int hasHit;
+    Transform target;
+    public float ownHealth = 30f;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake()
+    {
+        target = TargetPlayer.transform;
+        
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+
+        if (ownHealth <= 0)
+        { Destroy(gameObject); }
+
+        if (Vector3.Distance(transform.position, target.position) < 3f)
+        {
+            nav.enabled = false;
+            if (hasHit == 0)
+            {
+              
+                hasHit = 100;
+            }
+            else
+            { hasHit = hasHit - 1; }
+        }
+        else {
+            nav.enabled = true;
+            nav.SetDestination(target.position);
+        }
+
+        if (hasHit != 0)
+        { hasHit = hasHit - 1; }
+        
+    }
+
+   
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //  IEnumerator HitEffect()
+    // {
+
+    //     while (MyColor.color != stateColor)
+    //    {
+
+    //       Color LerpingColor = Color.Lerp(hitColor, stateColor, 100f * Time.deltaTime);
+    //    whatsbeinglerpedthen = LerpingColor;
+    //     MyColor.color = LerpingColor;
+    //     yield return null;
+    //  }
+
+
+
+    // Color hitColor = new Color(1f, 0f, 0f, 1f);
+    //Color stateColor = new Color(1f, 0.5f, 0.063f, 1f);
+
+    // }
---
+++ AGESProject2/Assets/Script/PlayerManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour {
+
+
+
+    
+
+    
+    int storedWaveRate = 40;
+    int CurrentWaveRate = 0;
+    public int deadplayers = 0;
+
+    [SerializeField]
+    int wave = 1;
+    [SerializeField]
+    int waveEnemies = 20;
+    int nextWaveEnemies = 45;
+
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    [SerializeField]
+    float TargetedDist = 0f;
+    [SerializeField]
+    GameObject trgetedPlayer;
+    [SerializeField]
+    int chosenspawn;
+
+   
+
+private void Awake() //if you need this script to find things do it in awake
+    {
+        
+     }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+	}
+
+    private void FixedUpdate()
+    {
+            if (wave < 8)
+            {
+                if (waveEnemies >= 1)
+                {
+                    if (CurrentWaveRate == 0)
+                    {
+                        Spawn();
+                        waveEnemies = waveEnemies - 1;
+                        CurrentWaveRate = storedWaveRate;
+                    }
+                    else { CurrentWaveRate = CurrentWaveRate - 1; }
+                }
+                else if (waveEnemies == 0)
+                {
+                    UpdateWaveRate();
+                }
+            }
+    }
+
+    void UpdateWaveRate()
+    {
+        if (wave <= 8)
+        {
+            wave = wave + 1;
+            storedWaveRate = storedWaveRate - 5;
+            waveEnemies = nextWaveEnemies;
+            nextWaveEnemies = nextWaveEnemies + 15;
+           
+        }
+        
+    }
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        chosenspawn = spawnPointIndex;
+    
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        EnemyMovement spawnedScript = enemy.GetComponent<EnemyMovement>();
+        
+
+    }
+    
+}
+
+
+//ok what now is left
+
+   
+
+    //nothing indicates to the player that the game has begun/that they can join and that the game has begun and that the players can no longer join.
+
+    //
